7fedf02ae2197d5d3a0daff1b8612481cbb95f76
Joe Cooper <josephcooper3@gmail.com>
2019-10-14 16:02:25 +0100

    adds and tests Developer class

---
+++ src/main/java/staff/techStaff/Developer.java
@@ -0,0 +1,10 @@
+package staff.techStaff;
+
+import staff.Employee;
+
+public class Developer extends Employee {
+
+    public Developer(String name, String nINumber, Double salary) {
+        super(name, nINumber, salary);
+    }
+}
---
+++ src/test/java/DeveloperTest.java
@@ -0,0 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeveloperTest {
+
+    Developer developer;
+
+    @Before
+    public void before(){
+        developer = new Developer("Pat Lee", "SJ723693D", 30000.00);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Pat Lee", developer.getName());
+    }
+
+    @Test
+    public void canGetNINumber(){
+        assertEquals("SJ723693D", developer.getNINumber());
+    }
+
+    @Test
+    public void canGetSalary(){
+        assertEquals(30000.00, developer.getSalary(), 2);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        developer.raiseSalary(0.02);
+        assertEquals(30600.00, developer.getSalary(), 2);
+    }
+
+    @Test
+    public void canPayBonus(){
+        assertEquals(300.00, developer.payBonus(), 2);
+    }
+
+}
